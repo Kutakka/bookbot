c4ccf638c2cefc38806db5ce4f9be3190b730934
Kutakka <daisoujou@gmail.com>
2025-03-19 11:18:35 -0400

    word counter

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
-        return file_contents
+        total_words = file_contents.split()
+        num_words = len(total_words)
+        return num_words
 
 def main():
-    print(get_book_text("/home/kutaka/projects/github.com/bookbot/books/frankenstein.txt"))
+    word_count = get_book_text("/home/kutaka/projects/github.com/bookbot/books/frankenstein.txt")
+    print(f"{word_count} words found in the document")
 
 main()
